e5281a6bdd7ad2afc9dc1b59704283e526223613
sarpisik <contact@sarpisik.com>
2019-07-18 15:45:30 +0300

    Edit next server config

--- www/next.config.js
+++ www/next.config.js
@@ -1,10 +1,6 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
-    return config
-  }
-}
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants'); // Get values from `next-server` package when building on now v2
