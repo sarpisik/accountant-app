6407e6324563252f1c3b80f4fae017752d45d8ca
sarpisik <contact@sarpisik.com>
2019-07-26 22:38:43 +0300

    Edit error handler

--- app/middleware/invoice/readInvoices.js
+++ app/middleware/invoice/readInvoices.js
@@ -4,7 +4,10 @@ const { Invoice } = require('../../models'),
     Invoice.find({ type }, keys, (error, invoices) => {
       if (error) return errorHandler(error, res);
       if (invoices.length < 1)
-        return errorHandler({ message: 'Can not find invoices.' }, res);
+        return errorHandler(
+          { message: 'Can not find invoices.', invoices },
+          res
+        );
       res.send({ invoices });
     });
 
